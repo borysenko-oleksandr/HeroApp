a4e4749628ecfcc760b2e590616e54a84a1250aa
Oleksandr Borysenko <oborysenko@pelmorex.com>
2026-07-01 12:01:49 +0300

    init project

---
+++ HeroApp/HeroAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeroAppApp.swift
+//  HeroApp
+//
+//  Created by User on 01.07.2026.
+//
+
+import SwiftUI
+
+@main
+struct HeroAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
